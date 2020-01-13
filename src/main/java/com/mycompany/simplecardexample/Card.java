7a6ac321da10a4faa3a1ba494683c7a1cc86496e
tanyi <tanyi@LAPTOP-4NF4QVGG>
2020-01-13 10:53:57 -0500

    my First commit

---
+++ src/main/java/com/mycompany/simplecardexample/Card.java
@@ -0,0 +1,48 @@
+/*
+Yi Tan <tan5@sheridan.desire2learn.com>
+----------------------------------------------------------------------
+ */
+package com.mycompany.simplecardexample;
+
+/**
+ *
+ * @author Yi Tan <tan5@sheridan.desire2learn.com>
+ */
+public class Card {
+    
+    private String suit;
+    private int value;
+
+    public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        if(suit.equals("Spades") || suit.equals("Diamond") || suit.equals("Clubs") || suit.equals("Hearts"))
+                 this.suit = suit;
+        else {
+            System.out.println("Incorrect suit. Please enter a valid suit");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        if(value >0 && value <14)
+            this.value = value;
+        else 
+        {
+            System.out.println("please enter a correct value ");
+        }
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/simplecardexample/DeckOfCards.java
@@ -0,0 +1,19 @@
+/*
+Yi Tan <tan5@sheridan.desire2learn.com>
+----------------------------------------------------------------------
+ */
+package com.mycompany.simplecardexample;
+
+/**
+ *
+ * @author Yi Tan <tan5@sheridan.desire2learn.com>
+ */
+public class DeckOfCards {
+    
+    public static void main(String[] args) {
+    
+    Card tenOfHearts = new Card("Hearts", 150); 
+    tenOfHearts.setValue(200);
+    
+    }
+}
